5a29a147de93e393f1c3d38dfcd6235bbbe4ac7b
Ivan Stepanov <ivan.stepanov08.1999@gmail.com>
2024-10-18 15:34:29 -0400

    main

--- Main.kt
+++ Main.kt
@@ -1,3 +1,8 @@
-fun main() {
-    println("Hello World!")
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            print("Hi")
+        }
+    }
 }
