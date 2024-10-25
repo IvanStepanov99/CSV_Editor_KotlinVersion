96df36fea16b4cbfdf5c927e55999adf1c1e889e
Ivan Stepanov <ivan.stepanov08.1999@gmail.com>
2024-10-25 16:55:55 -0400

    add writeToCsv method

--- src/CsvWriter.kt
+++ src/CsvWriter.kt
@@ -1,3 +1,4 @@
+import java.io.IOException
 import java.math.BigDecimal
 import java.util.Scanner
 
@@ -5,9 +6,28 @@ class CsvWriter{
 fun writeToCsv(path:String, keyboard: Scanner){
     try {
         do{
-            println("Plese enter income: ")
-            val expense = BigDecimal
-        }
+            println("Please enter income: ")
+            val income = BigDecimal(keyboard.nextLine())
+
+            println("Please enter expense: ")
+            val expense = BigDecimal(keyboard.nextLine())
+
+            println("Please enter debt/loan: ")
+            val debtLoan = BigDecimal(keyboard.nextLine())
+
+            println("Please enter interest: ")
+            val interest = BigDecimal(keyboard.nextLine())
+
+            println("Please enter type of loan: ")
+            val typeOfLoan = keyboard.nextLine()
+
+            println("Would you like to continue? yes/no")
+
+        }while (toContinue.equalsIgnoreCase("yes"))
+
+        println("Data succsessfully added to the $path")
+    }catch (e: IOException){
+        e.printStackTrace()
     }
 }
 }
